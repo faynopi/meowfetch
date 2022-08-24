663360c85a944a8192b2a726148a9d79971c3f31
Itsnexn <Itsnexn@tuta.io>
2022-08-24 16:12:51 +0430

    Quick FIX: shell widget only displaying "sh" #2
    This will resolve the issue with the shell widget return value, which
    is set after the shell environment variable is checked.

--- internals/widgets/shell.go
+++ internals/widgets/shell.go
@@ -1,53 +1,48 @@
 package widgets
 
 import (
-	config "meowfetch/configs"
-	"os"
-	"os/exec"
-	"path"
-	"strings"
+    config "meowfetch/configs"
+    "os"
+    "os/exec"
+    "path"
+    "strings"
 )
 
 func W_shell() string {
+    // default values
     var ret string
+    var shell string = "sh"
+    var shell_path string = "/usr/bin/sh"
 
     shell_path, status := os.LookupEnv("SHELL")
-
-    if !status {
-        return ""
+    if status {
+        shell = path.Base(shell_path)
     }
 
-    shell := path.Base(ret)
+    ret = shell
 
-    ret = shell_path
     if !*config.Shell_path {
         ret = shell
-    }
-
-    ret = "sh"
-    shell = "sh"
-    shell_path = "/usr/bin/sh"
-    if *config.Shell_version {
+    } else {
         switch shell {
-            case "bash":
-                ret += " " + os.Getenv("BASH_VERSION")
-            case "zsh":
-                ret += " " + os.Getenv("ZSH_VERSION")
-            case "yash":
-                out, err := exec.Command(shell_path, "--version").Output()
-                if err == nil {
-                    ret += " " + strings.Split(strings.Split(string(out), "\n")[0], " ")[4]
-                }
-            case "sh":
-                out, err := exec.Command(shell_path, "--version").Output()
-                if err == nil {
-                    ret += " " + strings.Split(strings.Split(string(out), "\n")[0], " ")[3]
-                }
-            case "osh":
-                ret += " " + os.Getenv("OIL_VERSION")
+        case "bash":
+            ret += " " + os.Getenv("BASH_VERSION")
+        case "zsh":
+            ret += " " + os.Getenv("ZSH_VERSION")
+        case "yash":
+            out, err := exec.Command(shell_path, "--version").Output()
+            if err == nil {
+                ret += " " + strings.Split(strings.Split(string(out), "\n")[0], " ")[4]
+            }
+        case "sh":
+            out, err := exec.Command(shell_path, "--version").Output()
+            if err == nil {
+                ret += " " + strings.Split(strings.Split(string(out), "\n")[0], " ")[3]
+            }
+        case "osh":
+            ret += " " + os.Getenv("OIL_VERSION")
         }
     }
-
 
     return ret
 }
